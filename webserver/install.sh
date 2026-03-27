80b016f926fd7bba763956580295de4d81a1958a
allenkwok11 <allenkwok11@users.noreply.github.com>
2026-03-27 13:17:58 +0800

    Add Ubuntu 22/24 webcontroller compatibility

--- webserver/install.sh
+++ webserver/install.sh
@@ -8,8 +8,16 @@ BASEDIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 echo WEBCONTROLLER_VERSION=\"$(cd $BASEDIR; ~/mini_pupper_bsp/get-version.sh)\" >> ~/mini-pupper-release
 
 source  ~/mini-pupper-release
+source /etc/os-release
+
+PIP_BREAK=""
+if [ "$UBUNTU_CODENAME" == "noble" ]
+then
+    PIP_BREAK="--break-system-packages"
+fi
 
 sudo rm -rf /usr/lib/python3/dist-packages/blinker*
+sudo apt-get install -y python3-pip
 if [ "$IS_RELEASE" == "YES" ]
 then
     cd $BASEDIR
@@ -20,14 +28,18 @@ then
         sed -i "s/IS_RELEASE=YES/IS_RELEASE=NO/" ~/mini-pupper-release
     fi
     VERSION=$(cd $BASEDIR; ~/mini_pupper_bsp/get-version.sh)
-    sudo PBR_VERSION=$VERSION pip install $BASEDIR/backend
-    sudo PBR_VERSION=$VERSION  pip install $BASEDIR/../joystick_sim
+    sudo PBR_VERSION=$VERSION python3 -m pip install $PIP_BREAK $BASEDIR/backend
+    sudo PBR_VERSION=$VERSION python3 -m pip install $PIP_BREAK $BASEDIR/../joystick_sim
 else
-    sudo pip install $BASEDIR/backend
-    sudo pip install $BASEDIR/../joystick_sim
+    sudo python3 -m pip install $PIP_BREAK $BASEDIR/backend
+    sudo python3 -m pip install $PIP_BREAK $BASEDIR/../joystick_sim
 fi
 
-sudo ln -s $BASEDIR/web-controller.service /etc/systemd/system/
+if ! python3 -c "import UDPComms" >/dev/null 2>&1; then
+    sudo python3 -m pip install $PIP_BREAK git+https://github.com/stanfordroboticsclub/UDPComms.git
+fi
+
+sudo ln -sf $BASEDIR/web-controller.service /etc/systemd/system/web-controller.service
 sudo systemctl daemon-reload
 sudo systemctl enable web-controller
 sudo systemctl start web-controller
